672ad1459da59b9f557f914695daea8143dd43be
IvanContador <ivcoso@floridauniversitaria.es>
2024-06-12 12:21:04 +0200

    ej3 hecho

---
+++ 3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+media=0
+cuantos=0
+total=0
+lineas=`cat datos.txt | wc -l`
+
+for i in `seq 2 $lineas`
+do
+  linea=`cat datos.txt | head -n$i | tail -n1`
+  temperatura=`echo $linea | awk '{print $2}'`
+  ciudad=`echo $linea | awk '{print $1}'`
+  
+  if [ $ciudad = $1 ]
+  then
+    total=$((total+temperatura))
+    cuantos=$((cuantos+1))
+  fi
+done
+
+if [ $total -eq 0 ]
+then
+  echo "La ciudad no existe "
+else
+  media=$((total/cuantos))
+  echo $media
+fi
